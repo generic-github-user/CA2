1dfdb4b7a656e017bd2013a87b5d9b548877873d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-22 01:58:23 -0400

    extract main include statements

---
+++ mainheaders.h
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <inttypes.h>
+#include "spng.h"
+//#include "TinyPngOut.h"
+
+#include "array.h"
+// #include "vector.h"
+#include "state.h"
+#include "simulation.h"
+#include "rule.h"
+#include "helpers.h"
+#include "commands.h"
+
+// Based on code from https://stackoverflow.com/a/3219471
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
+const char* COLOR_ORDER[6] = { RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA };
+
