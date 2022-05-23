df1f2d1827cec5e0a4f59dc443bb6e70a681daf2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-23 03:30:42 -0400

    add support for templates with arguments

--- build.py
+++ build.py
@@ -1,6 +1,18 @@
 import glob
 import os
+import re
 
+def expand(m):
+    print(f"Searching match {m[0]}")
+    args = re.search(r"(\w+)\(((?:\w+\:.+,?)+)\)", m[0])
+    tname = args[1]
+    params = args[2].split(",")
+    with open(f"./{tname.lower()}.ct", "r") as t:
+        template = t.read()
+    for p in params:
+        a, b = p.split(":")
+        template = template.replace(a.upper(), b)
+    return template
 for path in glob.glob("./*.c0"):
     with open(path, "r") as src:
         content = src.read()
@@ -9,8 +21,8 @@ for path in glob.glob("./*.c0"):
         with open(t, "r") as th:
             template = th.read()
             content = content.replace("{{"+name+"}}", template)
+    content = re.sub(r"\$(.*)\$", expand, content);
     with open(path.replace("c0", "c"), "w") as dest:
         dest.write(content)
-
 
 
