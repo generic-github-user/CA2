3828ecb109190709eaea924f9af6773c1cacf9b6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-23 02:23:10 -0400

    fix issues with build script

--- build.py
+++ build.py
@@ -2,12 +2,15 @@ import glob
 import os
 
 for path in glob.glob("./*.c0"):
-    with open(path, "w") as src:
+    with open(path, "r") as src:
         content = src.read()
-        for t in glob.glob("./*.ct"):
-            name = os.path.basename(t).split(".")[0]
-            with open(t, "r") as th:
-                template = th.read()
-                content = content.replace("{{"+name+"}}", template)
+    for t in glob.glob("./*.ct"):
+        name = os.path.basename(t).split(".")[0]
+        with open(t, "r") as th:
+            template = th.read()
+            content = content.replace("{{"+name+"}}", template)
+    with open(path.replace("c0", "c"), "w") as dest:
+        dest.write(content)
+
 
 
