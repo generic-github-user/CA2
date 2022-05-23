501f66283de9caa6f69113f5c6f640a8c705965d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-23 04:16:57 -0400

    Search for templates within subdirectories

--- build.py
+++ build.py
@@ -7,16 +7,17 @@ def expand(m):
     args = re.search(r"(\w+)\(((?:\w+\:.+,?)+)\)", m[0])
     tname = args[1]
     params = args[2].split(",")
-    with open(f"./{tname.lower()}.ct", "r") as t:
+    tpath = glob.glob(f"./**/{tname.lower()}.ct")[0]
+    with open(tpath, "r") as t:
         template = t.read()
     for p in params:
         a, b = p.split(":")
         template = template.replace(a.upper(), b)
     return template
-for path in glob.glob("./*.c0"):
+for path in glob.glob("./*/*.c0"):
     with open(path, "r") as src:
         content = src.read()
-    for t in glob.glob("./*.ct"):
+    for t in glob.glob("./*/*.ct"):
         name = os.path.basename(t).split(".")[0]
         with open(t, "r") as th:
             template = th.read()
