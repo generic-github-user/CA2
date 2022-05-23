d3cd40dcc83e210c10c39e1aa7d8356763dff550
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-22 21:41:56 -0400

    add simple plotting script for data serialized from program

---
+++ plot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import time
+
+# TODO
+time.sleep(1)
+def writelog(content):
+    with open("pylog.txt", "a") as log:
+        log.write(content+"\n")
+writelog('\n'*3)
+path = sys.argv[1]
+#sys.stdout.write(path)
+writelog(path)
+with open(path, "r") as f:
+    text = f.read()
+#    print(text)
+    sys.stdout.write(text)
+    writelog(text)
+    data = np.fromstring(text, dtype=int, sep=",")
+    
+    writelog("Generating plot")
+    plt.style.use("seaborn")
+    fig, ax = plt.subplots()
+    ax.imshow(data.reshape(30, 30))
+    fig.savefig("./ca_plot.png")
+    plt.close(fig)
+
