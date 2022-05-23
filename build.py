38f0ebb3cd2e72405b72c691de0d5db2dd4faabe
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-23 01:53:24 -0400

    add build script for generating processed C files from templates

---
+++ build.py
@@ -0,0 +1,13 @@
+import glob
+import os
+
+for path in glob.glob("./*.c0"):
+    with open(path, "w") as src:
+        content = src.read()
+        for t in glob.glob("./*.ct"):
+            name = os.path.basename(t).split(".")[0]
+            with open(t, "r") as th:
+                template = th.read()
+                content = content.replace("{{"+name+"}}", template)
+
+
