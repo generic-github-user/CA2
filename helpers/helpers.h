65bd06beb356a246ee3c921c6c32f9c49b7da1f1
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-23 23:08:48 -0400

    support use of format strings in printx

--- helpers/helpers.h
+++ helpers/helpers.h
@@ -4,7 +4,7 @@
 #include "../simulation/simulation.h"
 
 int inrange(int x, int n, int m);
-void printx(int, char*);
+void printx(int, const char*, ...);
 char* microplot(simulation);
 int streq(char*, char*);
 void assert(int, char*);
