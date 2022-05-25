7c9c47c3a4ffe7924cce4c35411f748b31a9043f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-25 18:31:40 -0400

    Add function for freeing memory used for storing a simulation

--- simulation/simulation.c
+++ simulation/simulation.c
@@ -1,5 +1,5 @@
-/* Generated from ./simulation/simulation.c0 at 05/25/2022, 03:26:15 */ 
-/* This is a content file generated from a source (.c0) file; you should edit that file instead */ 
+/* Generated from ./simulation/simulation.c0 at 05/25/2022, 03:26:15 */
+/* This is a content file generated from a source (.c0) file; you should edit that file instead */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -30,6 +30,14 @@ simulation* new_simulation(state s, int steps) {
 
 //	printf("Created new simulation
 	return sim;
+}
+
+// Clear the memory used by a simulation and its constituent members (mainly states)
+void free_sim(simulation* s) {
+	for (int i=0; i<s->time; i++) {
+//		free_state
+	}
+	free(s);
 }
 
 void sim_data(simulation s) {
