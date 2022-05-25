04c57b2e2433b76045cbc7d222bb986715594b01
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-25 03:45:44 -0400

    add missing include statements

--- typing/typing.c
+++ typing/typing.c
@@ -1,5 +1,9 @@
-/* Generated from ./typing/typing.c0 at 05/25/2022, 02:40:18 */ 
+/* Generated from ./typing/typing.c0 at 05/25/2022, 03:26:15 */ 
 /* This is a content file generated from a source (.c0) file; you should edit that file instead */ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 // A struct representing a generic type; instances of this struct describe specific types, which objects may be sampled from or compared against
 struct type {
 	char* name;
