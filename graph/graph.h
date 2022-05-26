2a053b3e3d624c32e41351ce1f482e8c44269f9f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-26 19:12:09 -0400

    add comments to graph.h

--- graph/graph.h
+++ graph/graph.h
@@ -1,8 +1,9 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+// A generic node with a pointer to a value and information about adjacent nodes
 struct node {
-	void* data;
+	void* data; // The data stored in the node -- the datatype and any necessary dereferencing will need to be handled by the user
 	struct node** in;
 	struct node** out;
 	unsigned int indegree;
@@ -10,12 +11,14 @@ struct node {
 };
 typedef struct node node;
 
+// A graph data structure containing a collection of nodes
 struct graph {
 	node* nodes;
 	int size;
 };
 typedef struct graph graph;
 
+// Function headers
 node new_node(graph*, void*);
 void add_in(node*, node*);
 node* new_list(void*);
