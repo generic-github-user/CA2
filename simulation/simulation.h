d3f1f04957cdaa308b0b748934da1aec2986582d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-25 23:51:51 -0400

    rewrite simulation code to store an array of state pointers

--- simulation/simulation.h
+++ simulation/simulation.h
@@ -8,7 +8,7 @@ typedef struct state state;
 
 // A series of frames along with a simulation rule that describes the transition from one state to another (possibly contains additional information)
 struct simulation {
-	state* states;
+	state** states;
 	int time, steps, compute;
 	array ages;
 	// Amount of space dynamically alloated for members of this struct
@@ -16,7 +16,7 @@ struct simulation {
 };
 typedef struct simulation simulation;
 
-simulation* new_simulation(state s, int steps);
+simulation* new_simulation(state* s, int steps);
 void sim_data(simulation s);
 void sim_summary(simulation*);
 char* sim_info(simulation);
