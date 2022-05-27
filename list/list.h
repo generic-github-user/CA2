02f46c381133cde09ee4e5626e208347220ab245
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-05-27 05:46:13 -0400

    add comments to list.h

--- list/list.h
+++ list/list.h
@@ -1,14 +1,15 @@
 struct list {
-	void** data;
-	int* free_cells;
-	int nfree;
-	int size;
-	int block_size;
-	int ordered;
-	int compute;
+	void** data; // the actual container that store's the list's data; a generic pointer
+	int* free_cells; // a list (stack) of available indices
+	int nfree; // the number of unused spaces reserved for the data
+	int size; // the number of elements in the list
+	int block_size; // the number of bytes to be (de)allocated when the list changes size
+	int ordered; // whether the list elements must be kept in the order they were added in; if 0, the struct acts as a multiset
+	int compute; // number of operations used by this list so far
 };
 typedef struct list list;
 
+// Function declarations (see list.c for details)
 list* new_llist(void**, int, int);
 void free_llist(list*, int);
 void llist_add(list*, void*);
