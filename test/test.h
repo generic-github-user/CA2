742bc9620f94a21dfc0f5257a18e37abb754d07e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-03 04:06:46 -0400

    add test struct field comments

--- test/test.h
+++ test/test.h
@@ -1,6 +1,6 @@
 struct test {
-	int passed;
-	struct test* subtests;
-	int nsub;
+	int passed; // a truth value indicating whether the test completed successfully
+	struct test* subtests; // a set of "member" tests that this test depends on
+	int nsub; // number of subtests
 };
 typedef struct test test;
