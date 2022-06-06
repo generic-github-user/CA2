8852e0a1df8f73d983d3ecf2ac5e05d73bdc339f
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-05 20:59:50 -0400

    track creation and modification times for states and simulations

--- simulation/simulation.c
+++ simulation/simulation.c
@@ -1,3 +1,5 @@
+/* Generated from simulation/simulation.c at 06/05/2022 */ 
+/* This is a content file generated from a source (.c0) file; you should edit that file instead */ 
 /* Generated from simulation/simulation.c0 at 05/27/2022 */ 
 /* This is a content file generated from a source (.c0) file; you should edit that file instead */ 
 #include <stdlib.h>
