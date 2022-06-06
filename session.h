f1366bf68d61d8d2ed752ce3d68a1bb9b3f7488c
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-06-05 21:50:01 -0400

    store timeinfo for session structs

--- session.h
+++ session.h
@@ -1,6 +1,9 @@
+#include "timeinfo.h"
+
 struct session {
 	void** selection; // The user's current selection
 	int space, compute; // Memory and operations used during this session
+	timeinfo time;
 };
 typedef struct session session;
 
